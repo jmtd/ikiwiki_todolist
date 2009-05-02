df9d0e2bce7a1bbf84d2dba1fc2b0520a3522457
Jon Dowland <jon@forelli.alcopop.org>
2009-05-02 01:01:19 +0100

    some stuff from relativedate to try and fix js include

--- IkiWiki/Plugin/jonlist.pm
+++ IkiWiki/Plugin/jonlist.pm
@@ -7,8 +7,11 @@ use strict;
 use IkiWiki 3.00;
 
 sub import {
+    add_underlay("javascript");
     hook(type => "htmlize", id => "jonlist", call => \&htmlize);
     hook(type => "pagetemplate", id => "jonlist", call => \&pagetemplate);
+    hook(type => "format", id => "jonlist", call => \&format);
+
 
 }
 
@@ -29,11 +32,11 @@ sub pagetemplate () {
 	my $page=$params{page};
     my $template = $params{template};
     my $cgiurl = $config{cgiurl};
-    my $url = urlto("outliner.js", $page);
+    my $url = urlto("outliner.js", $page, 1);
     my $doohicky = "<script lang=\"text/javascript\">
 	cgiurl = \"$cgiurl\";
     </script>
-    <script type="text/javascript" src=\"$url\" ></script>
+    <script type=\"text/javascript\" src=\"$url\" ></script>
 ";
 
     my $jonplugin = "jonvar";
@@ -42,5 +45,28 @@ sub pagetemplate () {
         $template->param(jonvar => $doohicky);
     }
 }
+
+# thanks to relativedate
+sub format (@) {
+        my %params=@_;
+
+        if (! ($params{content}=~s!^(<body>)!$1.include_javascript($params{page}
+)!em)) {
+                # no </body> tag, probably in preview mode
+                $params{content}=include_javascript($params{page}, 1).$params{co
+ntent};
+        }
+        return $params{content};
+}
+
+# thanks to relativedate
+sub include_javascript ($;$) {
+        my $page=shift;
+        my $absolute=shift;
+        
+        return '<script src="'.urlto("outliner.js", $page, $absolute).
+                '" type="text/javascript" charset="utf-8"></script>'."\n";
+}
+
 
 1
